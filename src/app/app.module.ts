0e073c8f1c3c28127e32d2da4e6ab1096df1432f
pertvirt <justforplaydota2@gmail.com>
2020-04-07 15:18:30 +0700

    New update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductRowComponent } from './product-row/product-row.component';
+import {AppComponent} from './app.component';
+import {ProductsListComponent} from './products-list/products-list.component';
+import {ProductRowComponent} from './product-row/product-row.component';
+import {ProductImageComponent} from './product-image/product-image.component';
+import {PriceDisplayComponent} from './price-display/price-display.component';
+import {ProductDepartmentComponent} from './product-department/product-department.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductRowComponent
+    ProductRowComponent,
+    ProductImageComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
